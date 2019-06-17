214e3e781def1dc396999cfb1d10feba5434e9f0
Chris Gardner <chris.gardner@section8games.co.uk>
2019-06-17 12:55:27 +0100

    Add stucco plaster template for common files

---
+++ psakeFile.ps1
@@ -0,0 +1,10 @@
+properties {
+    # Disable "compiling" module into monolithinc PSM1.
+    # This modifies the default behavior from the "Build" task
+    # in the PowerShellBuild shared psake task module
+    $PSBPreference.Build.CompileModule = $false
+}
+
+task default -depends Test
+
+task Test -FromModule PowerShellBuild -Version '0.3.0'
