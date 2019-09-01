731c603fed69c2c0bcc39876b2b6d8b63cd33b9c
Chris Gardner <chris.gardner@section8games.co.uk>
2019-09-01 22:27:12 +0100

    Add Resolve-ArmUtcNowFunction and tests

--- src/private/Resolve-ArmutcNowFunction.ps1
+++ src/private/Resolve-ArmutcNowFunction.ps1
@@ -1,10 +1,22 @@
 Function Resolve-ArmutcNowFunction {
     [cmdletbinding()]
     param (
-        [parameter(Mandatory)]
-        [ArmValue[]]$Arguments,
+        [parameter()]
+        [ArmStringValue]$Arguments,
 
         [parameter()]
         [TemplateRootAst]$Template
     )
+
+    if ($Template -isnot [TemplateParameterAst]) {
+        Write-Error -Message "You can't use UtcNow function except in the Parameters block of a tempalte"
+    }
+    else {
+        if ($Arguments) {
+            (Get-Date).ToString($Arguments.ToString())
+        }
+        else {
+            (Get-Date).ToString('yyyyMMddTHHmmssZ')
+        }
+    }
 }
