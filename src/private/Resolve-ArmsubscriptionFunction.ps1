d27a095f2509807e68848cc0b462c06b08d7ecf9
Chris Gardner <chris.gardner@section8games.co.uk>
2019-09-01 11:56:40 +0100

    Add Resolve-ArmSubscriptionFunction function

--- src/private/Resolve-ArmsubscriptionFunction.ps1
+++ src/private/Resolve-ArmsubscriptionFunction.ps1
@@ -1,10 +1,24 @@
 Function Resolve-ArmsubscriptionFunction {
     [cmdletbinding()]
     param (
-        [parameter(Mandatory)]
+        [parameter()]
         [ArmValue[]]$Arguments,
 
         [parameter()]
         [TemplateRootAst]$Template
     )
+
+    if (-not $Script:SubscriptionId) {
+        $Script:SubscriptionId = New-Guid | Select-Object -ExpandProperty Guid
+    }
+    if (-not $Script:TenantId) {
+        $Script:TenantId = New-Guid | Select-Object -ExpandProperty Guid
+    }
+
+    [PSCustomObject]@{
+        id = "/subscriptions/$($Script:SubscriptionId)"
+        subscriptionId = "$($Script:SubscriptionId)"
+        tenantId = "$($Script:TenantId)"
+        displayName = "name-of-subscription"
+    }
 }
