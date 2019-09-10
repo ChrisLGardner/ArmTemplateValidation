cc2aac2b95136325d7aad8eeab612cf3785bab23
Chris Gardner <chris.gardner@section8games.co.uk>
2019-09-10 20:32:15 +0100

    Add additional parameter tests to check for other properties

--- tests/unit/030-TemplateParameterAst.Tests.ps1
+++ tests/unit/030-TemplateParameterAst.Tests.ps1
@@ -11,10 +11,33 @@ InModuleScope ArmTemplateValidation {
                 $MissingType = [PSCustomObject]@{
                     DefaultValue = '1234'
                 }
+                $NormalParameter = [PSCustomObject]@{
+                    type = 'string'
+                }
+                $ValueParameter = [PSCustomObject]@{
+                    type = 'string'
+                    value = 'abc123'
+                }
+                $DefaultValueParameter = [PSCustomObject]@{
+                    type = 'string'
+                    defaultValue = 'def456'
+                }
             }
 
             It "Throws an error when required properties aren't provided" {
                 {[TemplateParameterAst]::New('MissingType', $MissingType, $EmptyParent)} | Should -Throw "Missing required properties, expected: Type"
+            }
+
+            It "Should not throw when a parameter with required properties is provided" {
+                {[TemplateParameterAst]::New('NormalParameter', $NormalParameter, $EmptyParent)} | Should -Not -Throw
+            }
+
+            It "Should set the value of the parameter when provided" {
+                ([TemplateParameterAst]::New('ValueParam', $ValueParameter, $EmptyParent)).Value | Should -Be 'abc123'
+            }
+
+            It "Should set the defaultValue of the parameter when provided" {
+                ([TemplateParameterAst]::New('DefaultValueParam', $DefaultValueParameter, $EmptyParent)).DefaultValue | Should -Be 'def456'
             }
 
         }
