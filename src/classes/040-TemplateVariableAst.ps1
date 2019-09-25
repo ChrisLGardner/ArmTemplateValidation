23ab9ff5be35f441373bd42a95846fbda0250722
Chris Gardner <chris.gardner@section8games.co.uk>
2019-09-25 11:25:31 +0100

    Remove extra method that has been moved to the TempalteAst base type

--- src/classes/040-TemplateVariableAst.ps1
+++ src/classes/040-TemplateVariableAst.ps1
@@ -21,17 +21,4 @@ class TemplateVariableAst : TemplateRootAst {
         }
     }
 
-    [PSObject] ResolveValue ([PSObject]$InputObject) {
-
-        foreach ($property in $InputObject.PSObject.Properties.Name) {
-            if ($InputObject.$property.GetType().Name -eq 'PSCustomObject') {
-                $InputObject.$Property = $this.ResolveValue($InputObject.$Property)
-            }
-            elseif ($InputObject.$Property -match '^\[') {
-                $InputObject.$property = Resolve-ArmFunction -InputString $InputObject.$property -Template ($this)
-            }
-        }
-
-        return $InputObject
-    }
 }
