6276949a9919ef077e7cbf0b2c3712fbc719f30b
Chris Gardner <chris.gardner@section8games.co.uk>
2019-09-25 21:16:52 +0100

    Update ResolveValue method to handle plain strings, arrays, and arrays inside objects better

--- src/classes/010-TemplateRootAst.ps1
+++ src/classes/010-TemplateRootAst.ps1
@@ -8,12 +8,27 @@ class TemplateRootAst {
 
     [PSObject] ResolveValue ([PSObject]$InputObject) {
 
-        foreach ($property in $InputObject.PSObject.Properties.Name) {
-            if ($InputObject.$property.GetType().Name -eq 'PSCustomObject') {
-                $InputObject.$Property = $this.ResolveValue($InputObject.$Property)
+        if ($InputObject -is [String] -and $InputObject -match '^\[') {
+            $InputObject = Resolve-ArmFunction -InputString $InputObject -Template ($this)
+        }
+        elseif ($InputObject -is [Array]) {
+            foreach ($item in $InputObject) {
+                $item = $this.ResolveValue($Item)
             }
-            elseif ($InputObject.$Property -match '^\[') {
-                $InputObject.$property = Resolve-ArmFunction -InputString $InputObject.$property -Template ($this)
+        }
+        elseif ($InputObject -isnot [String]) {
+            foreach ($property in $InputObject.PSObject.Properties.Name) {
+                if ($InputObject.$property.GetType().Name -eq 'PSCustomObject') {
+                    $InputObject.$Property = $this.ResolveValue($InputObject.$Property)
+                }
+                elseif ($InputObject.$property -is [Array]) {
+                    foreach ($item in $InputObject.$property) {
+                        $item = $this.ResolveValue($Item)
+                    }
+                }
+                elseif ($InputObject.$Property -match '^\[') {
+                    $InputObject.$property = Resolve-ArmFunction -InputString $InputObject.$property -Template ($this)
+                }
             }
         }
 
