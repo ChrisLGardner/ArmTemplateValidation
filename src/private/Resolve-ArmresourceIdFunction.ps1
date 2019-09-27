4bf6e7ce21c15cad28e382eb200e1ca77de4b112
Chris Gardner <chris.gardner@section8games.co.uk>
2019-09-27 16:31:22 +0100

    Update Resolve-ArmResourceIdFunction to return correct-ish id

--- src/private/Resolve-ArmresourceIdFunction.ps1
+++ src/private/Resolve-ArmresourceIdFunction.ps1
@@ -7,4 +7,23 @@ Function Resolve-ArmresourceIdFunction {
         [parameter()]
         [TemplateRootAst]$Template
     )
+
+    $Output = "$(Resolve-ArmResourceGroupFunction -Template $Template | Select-Object -ExpandProperty Id)/"
+    $strings = $Arguments.Token.StringValue.Where({$_ -notmatch '\/subscriptions\/'})
+
+    if ($strings[0] -match '(\/\w+){2,}') {
+        $type = $strings[0] -split '\/'
+        $parent = "$($type[0])/$($type[1])"
+        $Output += "$parent/"
+
+        for ($i = 1; $i -lt $strings.count; $i++) {
+            $Output += "$($strings[$i])/"
+            $Output += "$($Type[$i+1])/"
+        }
+    }
+    else {
+        $Output += $Strings -join '/'
+    }
+
+    $Output.TrimEnd('/')
 }
